140de59c3d88f5d5dd98fa365bb47c5bfe01ffc2
Gilberto Jr <giljrsantos@gmail.com>
2022-11-28 22:26:44 -0300

    implemetação funções privadas

--- product.js
+++ product.js
@@ -1,6 +1,48 @@
+import { EventEmitter } from 'events'
+
 export default class Product {
-    constructor({
-    onCreate,
-    service
-})
+    constructor({ 
+        onCreate, 
+        service
+      }) {
+        this.service = service
+        this.source = new EventEmitter()
+        this.source.on('create', onCreate)
+      }
+
+      // data = { description, id, price }
+      #isValid(data) {
+        if(data.description.length < 5) {
+          throw new Error('description must be higher than 5')
+        }
+      }
+
+      // data = { description, id, price }
+      #upperCaseStrings(data){
+        const finalObject = Reflect.ownKeys(data)
+        .map(key => {
+          const item = data[key]
+          return {
+            [key]: typeof item === 'string' ? item.toUpperCase() : item
+          }
+        })
+        .reduce((prev, next) => {
+          return {
+            ...prev,
+            ...next
+          }
+        }, {})
+
+        return finalObject
+      }
+      
+      async create(data){
+          this.#isValid(data)
+          const mappedObject = this.#upperCaseStrings(data)
+          console.log("🚀 ~ file: product.js ~ line 39 ~ Product ~ create ~ mapObject", mappedObject)
+          const message = await this.service.save(data)
+          this.source.emit('create', mappedObject)
+          
+          return message.toUpperCase()
+      }
 }
